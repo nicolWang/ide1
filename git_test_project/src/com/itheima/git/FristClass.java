820d8d9e61a96f2460c758f273fccee213318e4a
zhangsan <zhangsan@itcast>
2018-11-08 20:02:01 +0800

    aa

---
+++ git_test_project/src/com/itheima/git/FristClass.java
@@ -0,0 +1,22 @@
+package com.itheima.git;
+
+/**
+ * @Description: java类作用描述
+ * @Author: wn
+ * @CreateDate: 2018/11/8$ 19:56$
+ * @Version: 1.0
+ */
+
+public class FristClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
+
+
