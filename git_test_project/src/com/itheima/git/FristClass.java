a30ea86347cf3691698f737117a5cc809b49d740
zhangsan <zhangsan@itcast>
2018-11-08 20:06:42 +0800

    添加修改

--- git_test_project/src/com/itheima/git/FristClass.java
+++ git_test_project/src/com/itheima/git/FristClass.java
@@ -16,6 +16,7 @@ public class FristClass {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println(1+1);
     }
 }
 
